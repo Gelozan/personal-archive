670801dada0dce9423ed84e8c0282b60fdb3e691
Gelozan <olegegg@yandex.ru>
2026-05-13 20:45:18 +0300

    feat(backend): add create log function and actions list

---
+++ backend/app/core/actions.py
@@ -0,0 +1,32 @@
+# Auth
+ACTION_REGISTER         = "register"
+ACTION_LOGIN            = "login"
+ACTION_FORGOT_PASSWORD  = "forgot_password"
+ACTION_RESET_PASSWORD   = "reset_password"
+
+# User profile
+ACTION_CHANGE_NAME      = "user_change_name"
+ACTION_CHANGE_PASSWORD  = "user_change_password"
+
+# Documents
+ACTION_UPLOAD           = "document_upload"
+ACTION_UPDATE_METADATA  = "document_update_metadata"
+ACTION_MOVE_TO_TRASH    = "document_move_to_trash"
+ACTION_RESTORE          = "document_restore"
+ACTION_DELETE_PERMANENT = "document_delete_permanent"
+ACTION_DOWNLOAD         = "document_download"
+
+# Folders
+ACTION_FOLDER_CREATE    = "folder_create"
+ACTION_FOLDER_UPDATE    = "folder_update"
+ACTION_FOLDER_DELETE    = "folder_delete"
+
+# Categories
+ACTION_CATEGORY_CREATE  = "category_create"
+ACTION_CATEGORY_DELETE  = "category_delete"
+ACTION_CATEGORY_HIDE    = "category_hide_system"
+
+# Share
+ACTION_CREATE_SHARE     = "share_create"
+ACTION_REVOKE_SHARE     = "share_revoke"
+ACTION_ACCESS_SHARE     = "share_access"
